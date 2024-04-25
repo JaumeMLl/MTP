491d76752e7701adff4f0cc0d3b0227a46d3846e
Jaume Motje <jaumemotje00@gmail.com>
2024-04-25 22:13:24 +0200

    Nuevas versiones funciones anySupplicant y sendRequestAcc

--- NetworkMode/CommsMethods.py
+++ NetworkMode/CommsMethods.py
@@ -1,23 +1,19 @@
 import time
 
-from circuitpython_nrf24l01.rf24 import RF24
-
-import time
-
-def wait_for_desired_message(comms_info, desired_message, timeout):
+def wait_for_desired_message(comms_info, desired_message, timeout, nrf):
     """
     Waits until it receives the message matching the desired value on the given pipe, or until the timeout_ms expires.
     
     Parameters:
-    - listening_pipe_adress: The pipe being listened to for incoming messages.
-    - responder_pipe_adress: The pipe where the response should be sent.
+    - listening_pipe_address: The pipe being listened to for incoming messages.
+    - responder_pipe_address: The pipe where the response should be sent.
     - desired_message: The specific message expected to be received.
     - timeout: Timeout period in seconds.
 
     Returns:
     - True if the desired_message is received within the timeout period, False otherwise.
     """
-    nrf.open_rx_pipe(1, comms_info.listening_pipe_adress)
+    nrf.open_rx_pipe(1, comms_info.listening_pipe_address)
     nrf.listen = True  # put radio into RX mode and power up
 
     start = time.monotonic()
@@ -29,7 +25,7 @@ def wait_for_desired_message(comms_info, desired_message, timeout):
             received_message = nrf.read()
 
             # Extract the responder ID and the actual message from the received message
-            comms_info.responder_pipe_adress = received_message[:5]
+            comms_info.responder_pipe_address = received_message[:5]
             actual_message = received_message[5+2:]  # Skip 5 bytes for responder ID and 2 bytes for separator ": "
 
             # Check if the responder ID matches and the actual message is the desired one
