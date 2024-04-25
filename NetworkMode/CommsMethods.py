3167576105d8477074af3aa78c154ff10cfa5adc
Jaume Motje <jaumemotje00@gmail.com>
2024-04-25 23:29:08 +0200

    Añadimos send_message en CommsMethods

--- NetworkMode/CommsMethods.py
+++ NetworkMode/CommsMethods.py
@@ -5,15 +5,15 @@ def wait_for_desired_message(comms_info, desired_message, timeout, nrf):
     Waits until it receives the message matching the desired value on the given pipe, or until the timeout_ms expires.
     
     Parameters:
-    - listening_pipe_address: The pipe being listened to for incoming messages.
-    - responder_pipe_address: The pipe where the response should be sent.
+    - origin_pipe_address: The pipe being listened to for incoming messages.
+    - destination_pipe_address: The pipe where the response should be sent.
     - desired_message: The specific message expected to be received.
     - timeout: Timeout period in seconds.
 
     Returns:
     - True if the desired_message is received within the timeout period, False otherwise.
     """
-    nrf.open_rx_pipe(1, comms_info.listening_pipe_address)
+    nrf.open_rx_pipe(1, comms_info.origin_pipe_address)
     nrf.listen = True  # put radio into RX mode and power up
 
     start = time.monotonic()
@@ -25,7 +25,7 @@ def wait_for_desired_message(comms_info, desired_message, timeout, nrf):
             received_message = nrf.read()
             print(f"Received Message: {received_message}")
             # Extract the responder ID and the actual message from the received message
-            comms_info.responder_pipe_address = received_message[:5]
+            comms_info.destination_pipe_address = received_message[:5]
             actual_message = received_message[5+2:]  # Skip 5 bytes for responder ID and 2 bytes for separator ": "
 
             # Check if the responder ID matches and the actual message is the desired one
@@ -38,3 +38,19 @@ def wait_for_desired_message(comms_info, desired_message, timeout, nrf):
     
     print("Message not received")
     return False
+
+def send_message(comms_info, message, nrf):
+    print(f"Channel Information: {comms_info}")
+
+    nrf.listen = False  # Dejar de escuchar para poder enviar
+    nrf.open_tx_pipe(comms_info.destination_pipe_address)
+    data_to_send = comms_info.origin_pipe_address+b": "+ message
+    print(f"About to send: {data_to_send}")
+    sent_successfully = nrf.send(data_to_send)  # Enviar el mensaje de confirmación
+   
+    if sent_successfully:
+        print(f"{message} sent successfully.")
+    else:
+        print(f"Failed to send {message}.")
+
+    nrf.listen = True  # Volver al modo de escucha después de enviar
