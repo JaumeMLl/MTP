2ea22cf2cfb2fbb7b3963dd2ac39b7683cc3757c
Jaume Motje <jaumemotje00@gmail.com>
2024-04-26 15:36:36 +0200

    Implementamos las demas funciones. Verion1

--- NetworkMode/CommsMethods.py
+++ NetworkMode/CommsMethods.py
@@ -31,27 +31,27 @@ def wait_for_desired_message(comms_info, desired_message, timeout, nrf):
 
             # Check if the responder ID matches and the actual message is the desired one
             if actual_message == desired_message:
-                print("Message received")
+                print(f"Desired Message {desired_message} received")
                 return True  # Message received successfully
             else:
                 print("Message not recognized")
         time.sleep(0.1)  # Wait for a short time before checking again
     
-    print("Message not received")
+    print(f"Desired Message {desired_message} NOT received")
     return False
 
-def send_message(comms_info, message, nrf):
-    print(f"Channel Information: {comms_info}")
-
+def send_message(destination_pipe_address, message, nrf):
     nrf.listen = False  # Dejar de escuchar para poder enviar
-    nrf.open_tx_pipe(comms_info.destination_pipe_address)
+    nrf.open_tx_pipe(destination_pipe_address)
     data_to_send = MY_PIPE_ID+b": "+ message
     print(f"About to send: {data_to_send}")
     sent_successfully = nrf.send(data_to_send)  # Enviar el mensaje de confirmación
    
     if sent_successfully:
         print(f"{message} sent successfully.")
+        return True
     else:
         print(f"Failed to send {message}.")
+        return False
 
     nrf.listen = True  # Volver al modo de escucha después de enviar
