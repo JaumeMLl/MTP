e52520a60bf7ee7146669ea916b2847726367d61
manelcardenas <joan.manel.cardenas@estudiantat.upc.edu>
2024-05-01 18:26:53 +0200

    canal issue 8

--- Single_Mode/Single_Mode.py
+++ Single_Mode/Single_Mode.py
@@ -135,12 +135,7 @@ def master(filelist, count=5):
     message = open(filepath, 'rb').read() + b'separaciofitxer' + bytes(filepath.split('/')[-1], 'utf-8')
 
     chunks = [message[i:i + 32] for i in range(0, len(message), 32)]
-
-    if nrf.is_tx_fifo_full():
-        print("The transmission buffer is full.")
-    else:
-        print("The transmission buffer is not full.")
-
+    
     result = nrf.send(b'Ready')
     while not result:
         time.sleep(0.1)
